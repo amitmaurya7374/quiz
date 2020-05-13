def2e90fcf50916408c197f207d69db283f36871
amitmaurya7374 <amitmaurya7374@gmail.com>
2020-05-13 15:48:07 +0530

    second commit

---
+++ lib/model/question_model.dart
@@ -0,0 +1,6 @@
+class Question{
+  String questionText;//instance variable or member variable
+  bool isCorrect;
+  Question({this.questionText,this.isCorrect});//create an real life object
+  
+}
--- lib/quiz_screen.dart
+++ lib/quiz_screen.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
+
 class QuizScreen extends StatefulWidget {
   @override
   _QuizScreenState createState() => _QuizScreenState();
 }
 
 class _QuizScreenState extends State<QuizScreen> {
+  List questionBank=[
+    'The Republic of India is the largest country in South Asia.'
+  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('True Citizen'),
+        centerTitle: true,
+        backgroundColor: Colors.blueGrey,
+      ),
+      backgroundColor: Colors.blueGrey,
+      body: Container(
+        child: Column(
+          // crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Center(
+              child: Image.asset(
+                'assets/images/flag.jpg',
+                width: 350,
+                height: 350,
+              ),
+            ),
+            Container(
+              height: 120.0,
+              child: Text(questionBank.elementAt(0)),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }

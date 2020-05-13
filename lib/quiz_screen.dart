f9d67742fb23e62de08d3a8a802391166536e1b1
amitmaurya7374 <amitmaurya7374@gmail.com>
2020-05-13 16:32:11 +0530

    third commit

--- lib/quiz_screen.dart
+++ lib/quiz_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/model/question_model.dart';
 
 class QuizScreen extends StatefulWidget {
   @override
@@ -6,8 +7,44 @@ class QuizScreen extends StatefulWidget {
 }
 
 class _QuizScreenState extends State<QuizScreen> {
-  List questionBank=[
-    'The Republic of India is the largest country in South Asia.'
+  int _currentQuestionIndex = 0;
+  List questionBank = [
+    Question(
+        questionText:
+            'The Republic of India is the largest country in South Asia.',
+        isCorrect: true),
+    Question(
+        questionText:
+            'India is the second most populous democracy in the world.',
+        isCorrect: false),
+    Question(
+        questionText: 'Mumbai is the capital city of India', isCorrect: false),
+    Question(
+        questionText: 'India is surrounded by Indian Ocean in the south.',
+        isCorrect: true),
+    Question(
+        questionText:
+            'India shares borders with 6 countries including Indonesia.',
+        isCorrect: false),
+    Question(
+        questionText:
+            'India is a republic consisting of 28 states and seven union territories with a two party system of democracy.',
+        isCorrect: false),
+    Question(
+        questionText: 'India has the world’s twelfth largest economy.',
+        isCorrect: false),
+    Question(
+        questionText:
+            'Poverty, illiteracy and malnutrition have mostly been eradicated due to  economic reforms since 1991.',
+        isCorrect: false),
+    Question(
+        questionText:
+            'Four major world religions, Hinduism, Buddhism, Jainism and Sikhism originated in India.',
+        isCorrect: true),
+    Question(
+        questionText:
+            'English is one of the official languages of the country since it is a "lingua franca.',
+        isCorrect: true),
   ];
   @override
   Widget build(BuildContext context) {
@@ -30,12 +67,75 @@ class _QuizScreenState extends State<QuizScreen> {
               ),
             ),
             Container(
+              margin: EdgeInsets.all(15),
               height: 120.0,
-              child: Text(questionBank.elementAt(0)),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(14.0),
+                  color: Colors.transparent,
+                  border: Border.all(width: 1, color: Colors.white)),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                  child: Text(
+                    questionBank[_currentQuestionIndex].questionText,
+                    style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontStyle: FontStyle.italic),
+                  ),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                RaisedButton(
+                  elevation: 6.0,
+                  color: Colors.blueGrey.shade900,
+                  onPressed: () => _checkAnswer(true),
+                  child: Text(
+                    'True',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                RaisedButton(
+                  elevation: 6.0,
+                  color: Colors.blueGrey.shade900,
+                  onPressed: () => _checkAnswer(false),
+                  child: Text(
+                    'False',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                RaisedButton(
+                    elevation: 6.0,
+                    color: Colors.blueGrey.shade900,
+                    onPressed: () => _nextQuestion(),
+                    child: Icon(
+                      Icons.arrow_forward_ios,
+                      color: Colors.white,
+                    )),
+              ],
             ),
           ],
         ),
       ),
     );
+  }
+
+  _checkAnswer(bool userChoice) {
+    if(userChoice == questionBank[_currentQuestionIndex].isCorrect){
+      //Correct
+      debugPrint("Yes it is true");
+    }else{
+      debugPrint('Incorrect');
+    }
+  }
+
+  _nextQuestion() {
+    setState(() {
+      _currentQuestionIndex =(_currentQuestionIndex + 1)%questionBank.length;
+    });
   }
 }

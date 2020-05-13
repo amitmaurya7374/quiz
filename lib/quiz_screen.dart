d399b95e3ed535d13e6d5e52b5e6d724ac2b34ad
amitmaurya7374 <amitmaurya7374@gmail.com>
2020-05-13 15:05:43 +0530

    first commit

---
+++ lib/quiz_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+class QuizScreen extends StatefulWidget {
+  @override
+  _QuizScreenState createState() => _QuizScreenState();
+}
+
+class _QuizScreenState extends State<QuizScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
